3821dc0d5f23e3396926d28907b0f4ad6446946a
tgstyle <tg.style@gmail.com>
2020-01-27 23:17:22 -0700

    Commit for Merge

--- src/main/java/ferro2000/immersivetech/common/blocks/metal/tileentities/TileEntityDistiller.java
+++ src/main/java/ferro2000/immersivetech/common/blocks/metal/tileentities/TileEntityDistiller.java
@@ -49,8 +49,9 @@ public class TileEntityDistiller extends TileEntityMultiblockMetal<TileEntityDis
 		super.readCustomNBT(nbt, descPacket);
 		tanks[0].readFromNBT(nbt.getCompoundTag("tank0"));
 		tanks[1].readFromNBT(nbt.getCompoundTag("tank1"));
-		if(!descPacket) inventory = Utils.readInventory(nbt.getTagList("inventory", 10), 4);
+		if(!descPacket) inventory = Utils.readInventory(nbt.getTagList("inventory", 10), 6);
 	}
+
 	@Override
 	public void writeCustomNBT(NBTTagCompound nbt, boolean descPacket) {
 		super.writeCustomNBT(nbt, descPacket);
