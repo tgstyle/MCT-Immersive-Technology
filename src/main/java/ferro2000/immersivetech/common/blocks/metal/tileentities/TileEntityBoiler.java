73a01d02f6da51e13af7e7c26c299edd71cf3602
Kurtchekov <kurtchekov@gmail.com>
2020-01-29 11:23:10 +0200

    Revamp of the audio system
    Crashes and weird sound behavior should be finally gone now.

--- src/main/java/ferro2000/immersivetech/common/blocks/metal/tileentities/TileEntityBoiler.java
+++ src/main/java/ferro2000/immersivetech/common/blocks/metal/tileentities/TileEntityBoiler.java
@@ -21,6 +21,7 @@ import ferro2000.immersivetech.common.blocks.metal.multiblocks.MultiblockBoiler;
 
 import ferro2000.immersivetech.common.util.ITSoundHandler;
 import ferro2000.immersivetech.common.util.ITSounds;
+import ferro2000.immersivetech.common.util.network.MessageStopSound;
 import ferro2000.immersivetech.common.util.network.MessageTileSync;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
@@ -51,8 +52,6 @@ public class TileEntityBoiler extends TileEntityMultiblockMetal<TileEntityBoiler
 	private static int inputFuelTankSize = ITConfig.Machines.boiler_fuel_tankSize;
 	private static int inputTankSize = ITConfig.Machines.boiler_input_tankSize;
 	private static int outputTankSize = ITConfig.Machines.boiler_output_tankSize;
-
-	private ITSoundHandler runningSound;
 
 	public FluidTank[] tanks = new FluidTank[] {
 		new FluidTank(inputFuelTankSize), 
@@ -134,15 +133,30 @@ public class TileEntityBoiler extends TileEntityMultiblockMetal<TileEntityBoiler
 	}
 
 	public void handleSounds() {
-		if (runningSound == null) runningSound = new ITSoundHandler(this, ITSounds.boiler, SoundCategory.BLOCKS, true, 2, 1, getPos());
 		BlockPos center = getPos();
-		EntityPlayerSP player = Minecraft.getMinecraft().player;
-		float attenuation = Math.max((float) player.getDistanceSq(center.getX(), center.getY(), center.getZ()) / 8, 1);
 		float level = (float) (heatLevel / ITConfig.Machines.boiler_workingHeatLevel);
-		runningSound.updatePitch(level);
-		runningSound.updateVolume((2 * level) / attenuation);
-		if (level > 0) runningSound.playSound();
-		else runningSound.stopSound();
+		if (level == 0) ITSoundHandler.StopSound(center);
+		else {
+			EntityPlayerSP player = Minecraft.getMinecraft().player;
+			float attenuation = Math.max((float) player.getDistanceSq(center.getX(), center.getY(), center.getZ()) / 8, 1);
+			ITSoundHandler.PlaySound(center, ITSounds.boiler, SoundCategory.BLOCKS, true, (2 * level) / attenuation, level);
+		}
+	}
+
+	@Override
+	public void onChunkUnload() {
+		if (!isDummy()) ITSoundHandler.StopSound(getPos());
+		super.onChunkUnload();
+	}
+
+	@Override
+	public void disassemble() {
+		if (!isDummy()) {
+			NBTTagCompound tag = new NBTTagCompound();
+			BlockPos center = getPos();
+			ImmersiveTech.packetHandler.sendToAllTracking(new MessageStopSound(center), new NetworkRegistry.TargetPoint(world.provider.getDimension(), center.getX(), center.getY(), center.getZ(), 0));
+		}
+		super.disassemble();
 	}
 
 	@Override
