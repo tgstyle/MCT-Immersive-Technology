9bae39056740139ba6d0e9d68b54691059c3b992
tgstyle <tg.style@gmail.com>
2020-01-28 23:46:33 -0700

    Remove Scala Dependency

--- src/main/java/ferro2000/immersivetech/common/util/ITSoundHandler.java
+++ src/main/java/ferro2000/immersivetech/common/util/ITSoundHandler.java
@@ -9,15 +9,12 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 
-import scala.actors.threadpool.locks.ReentrantLock;
-
 import java.util.List;
 
 public class ITSoundHandler extends PositionedSound implements ITickableSound {
 
     TileEntity tileEntity;
     private static List<ISound> playingSounds = Lists.newArrayList();
-    ReentrantLock lock = new ReentrantLock();
 
     public static void PlaySound(BlockPos posIn, SoundEvent soundIn, SoundCategory categoryIn, boolean repeatIn, float volumeIn, float pitchIn) {
         ITSoundHandler sound = playingSounds.get(posIn);
@@ -56,10 +53,8 @@ public class ITSoundHandler extends PositionedSound implements ITickableSound {
     }
 
     public void playSound() {
-        if(lock.isLocked()) throw new RuntimeException("this is not supposed to be multithreaded!!!");
-        lock.lock();
         SoundHandler handler = Minecraft.getMinecraft().getSoundHandler();
-        if(!playingSounds.contains(this)) {
+        if (!playingSounds.contains(this)) {
             playingSounds.add(this);
             try {
                 handler.playSound(this);
@@ -67,18 +62,14 @@ public class ITSoundHandler extends PositionedSound implements ITickableSound {
                 handler.stopSound(this);
             }
         }
-        lock.unlock();
     }
 
     public void stopSound() {
-        if(lock.isLocked()) throw new RuntimeException("this is not supposed to be multithreaded!!!");
-        lock.lock();
         SoundHandler handler = Minecraft.getMinecraft().getSoundHandler();
-        if(playingSounds.contains(this)) {
+        if (playingSounds.contains(this)) {
             playingSounds.remove(this);
             handler.stopSound(this);
         }
-        lock.unlock();
     }
 
     private void stopSound() {
