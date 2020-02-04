c09dfd04297f74daf3497dea19aac7b654e03e0e
tgstyle <tg.style@gmail.com>
2020-02-03 18:55:52 -0700

    Fluid changes
    Fixed Boiler Input for Fluids
    Changed Fluids to colorable fluids
    Added OSD for Item Trash

---
+++ src/main/java/ferro2000/immersivetech/common/fluid/FluidColored.java
@@ -0,0 +1,47 @@
+package ferro2000.immersivetech.common.fluid;
+
+import ferro2000.immersivetech.ImmersiveTech;
+import ferro2000.immersivetech.common.util.ITLogger;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
+public class FluidColored extends Fluid {
+	static final ResourceLocation ICON_MetalStill = new ResourceLocation("tconstruct:blocks/fluids/molten_metal");
+	static final ResourceLocation ICON_MetalFlowing = new ResourceLocation("tconstruct:blocks/fluids/molten_metal_flow");
+	static final ResourceLocation ICON_Still = new ResourceLocation(ImmersiveTech.MODID + ":fluids/fluid_still");
+	static final ResourceLocation ICON_Flowing = new ResourceLocation(ImmersiveTech.MODID + ":fluids/fluid_flowing");
+
+	int color;
+	public FluidColored(String name, int color, int temp, int density, int viscosity) {
+		super(name, ICON_MetalStill, ICON_MetalFlowing);
+		if(FluidRegistry.isFluidRegistered(name) == false) {
+			this.color = color;
+			this.setTemperature(temp);
+			this.setDensity(density);
+			this.setViscosity(viscosity);
+			FluidRegistry.registerFluid(this);
+			FluidRegistry.addBucketForFluid(this);
+			ITLogger.info("Added molten fluid: " + ImmersiveTech.MODID + ":" + name);
+		}
+	}
+
+	public FluidColored(String name, int color, int density, int viscosity, boolean gaseous) {
+		super(name, ICON_Still, ICON_Flowing);
+		if(FluidRegistry.isFluidRegistered(name) == false) {
+			this.color = color;
+			this.setDensity(density);
+			this.setViscosity(viscosity);
+			this.setGaseous(gaseous);
+			FluidRegistry.registerFluid(this);
+			FluidRegistry.addBucketForFluid(this);
+			ITLogger.info("Added fluid: " + ImmersiveTech.MODID + ":" + name);
+		}
+	}
+
+	@Override
+	public int getColor() {
+		return color|0xff000000;
+	}
+}
