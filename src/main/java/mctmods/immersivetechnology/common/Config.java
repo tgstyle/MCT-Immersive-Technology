ca8dd46c6662591b462529a29248ed5bb9899ae2
Kurtchekov <kurtchekov@gmail.com>
2020-02-22 01:43:05 +0200

    Transfer rate for IE pipes can now be configured

--- src/main/java/mctmods/immersivetechnology/common/Config.java
+++ src/main/java/mctmods/immersivetechnology/common/Config.java
@@ -139,6 +139,10 @@ public class Config {
 		public static class Experimental {
 			@Comment({"Replace IE pipes with IT's own version. [Default=true]"})
 			public static boolean replace_IE_pipes = true;
+			@Comment({"How much should the pipes be capable of transfering, in mb. [Default=50]"})
+			public static int pipe_transfer_rate = 50;
+			@Comment({"How much should the pipes be capable of transfering when pressurized, in mb. [Default=1000]"})
+			public static int pipe_pressurized_transfer_rate = 1000;
 		}
 	}
 }
