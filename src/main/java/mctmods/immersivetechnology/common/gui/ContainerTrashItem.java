0858acf3d475e880ed9e44c06aa5e47cd92464f1
tgstyle <tg.style@gmail.com>
2020-02-22 15:46:04 -0700

    Better yet let's just get everyone on the same page
    Should probably do this for all TE slots

--- src/main/java/mctmods/immersivetechnology/common/gui/ContainerTrashItem.java
+++ src/main/java/mctmods/immersivetechnology/common/gui/ContainerTrashItem.java
@@ -10,16 +10,10 @@ public class ContainerTrashItem extends ContainerIEBase<TileEntityTrashItem> {
 	public ContainerTrashItem(InventoryPlayer inventoryPlayer, TileEntityTrashItem tile) {
 		super(inventoryPlayer, tile);
 
-		this.addSlotToContainer(new Slot(this.inv, 0, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 1, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 2, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 3, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 4, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 5, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 6, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 7, 80, 34));
-		this.addSlotToContainer(new Slot(this.inv, 8, 80, 34));
-		slotCount=9;
+		slotCount = TileEntityTrashItem.slotCount;
+		for(int slot = 0; slot < slotCount; slot++) {
+			this.addSlotToContainer(new Slot(this.inv, slot, 80, 34));
+		}
 
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 9; j++) {
