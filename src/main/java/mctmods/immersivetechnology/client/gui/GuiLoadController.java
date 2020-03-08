6a89cf9173aedd8c54e905e0ff6243cdc9c03ae6
Kurtchekov <kurtchekov@gmail.com>
2020-03-08 21:06:34 +0200

    Unified valve gui codes, no longer pauses game when open, numpad now works

--- src/main/java/mctmods/immersivetechnology/client/gui/GuiLoadController.java
+++ src/main/java/mctmods/immersivetechnology/client/gui/GuiLoadController.java
@@ -1,26 +1,15 @@
 package mctmods.immersivetechnology.client.gui;
 
 import blusunrize.immersiveengineering.client.ClientUtils;
-import mctmods.immersivetechnology.ImmersiveTechnology;
 import mctmods.immersivetechnology.common.tileentities.TileEntityLoadController;
 import mctmods.immersivetechnology.common.util.TranslationKey;
-import mctmods.immersivetechnology.common.util.network.MessageTileSync;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.nbt.NBTTagCompound;
-import org.lwjgl.input.Keyboard;
 
 import java.awt.*;
-import java.io.IOException;
 
-public class GuiLoadController extends GuiScreen {
-
-    TileEntityLoadController tile;
-    GuiTextField limitPacket;
-    GuiTextField limitTime;
-    GuiTextField destinationKeep;
+public class GuiLoadController extends GUICommonValve {
 
     public GuiLoadController(TileEntityLoadController tile) {
         this.tile = tile;
@@ -38,23 +27,6 @@ public class GuiLoadController extends GuiScreen {
         destinationKeep.setText(tile.keepSize >= 0? String.valueOf(tile.keepSize) : "");
     }
 
-    public static int safeStringToInt(String string) {
-        try {
-            return Integer.parseInt(string);
-        } catch(NumberFormatException e) {
-            return 0;
-        }
-    }
-
-    @Override
-    public void onGuiClosed() {
-        NBTTagCompound tag = new NBTTagCompound();
-        tag.setInteger("packetLimit", !limitPacket.getText().equals("") ? safeStringToInt(limitPacket.getText()) : -1);
-        tag.setInteger("timeLimit", !limitTime.getText().equals("") ? safeStringToInt(limitTime.getText()) : -1);
-        tag.setInteger("keepSize", !destinationKeep.getText().equals("") ? safeStringToInt(destinationKeep.getText()) : -1);
-        ImmersiveTechnology.packetHandler.sendToServer(new MessageTileSync(tile, tag));
-    }
-
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         ScaledResolution resolution = new ScaledResolution(Minecraft.getMinecraft());
@@ -69,38 +41,5 @@ public class GuiLoadController extends GuiScreen {
         destinationKeep.drawTextBox();
         drawString(this.fontRenderer, TranslationKey.GUI_LOAD_CONTROLLER_LIMIT_DESTINATION.text(), width / 2 - 30, height / 2 + 23, Color.WHITE.getRGB());
         super.drawScreen(mouseX, mouseY, partialTicks);
-    }
-
-    @Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-        limitPacket.mouseClicked(mouseX, mouseY, mouseButton);
-        limitTime.mouseClicked(mouseX, mouseY, mouseButton);
-        destinationKeep.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-
-    @Override
-    protected void keyTyped(char typedChar, int keyCode) throws IOException {
-        if (keyCode == Keyboard.KEY_TAB) {
-            if (limitPacket.isFocused()) {
-                limitPacket.setFocused(false);
-                limitTime.setFocused(true);
-                destinationKeep.setFocused(false);
-            } else if (limitTime.isFocused()) {
-                limitPacket.setFocused(false);
-                limitTime.setFocused(false);
-                destinationKeep.setFocused(true);
-            } else {
-                limitPacket.setFocused(true);
-                limitTime.setFocused(false);
-                destinationKeep.setFocused(false);
-            }
-        } else if (keyCode == Keyboard.KEY_E || keyCode == Keyboard.KEY_ESCAPE) {
-            super.keyTyped(typedChar, 1);
-        } else if (keyCode >= 2 && keyCode <= 11 || keyCode == Keyboard.KEY_BACK || keyCode == Keyboard.KEY_DELETE || keyCode == Keyboard.KEY_LEFT || keyCode == Keyboard.KEY_RIGHT) {
-            limitPacket.textboxKeyTyped(typedChar, keyCode);
-            limitTime.textboxKeyTyped(typedChar, keyCode);
-            destinationKeep.textboxKeyTyped(typedChar, keyCode);
-        }
     }
 }
