5d9c21a1c08f00cbca7c7bbf107ded14e4d93b1a
tgstyle <tg.style@gmail.com>
2020-03-21 15:40:14 -0600

    Fixes for Barrels and Tank
    Optimizations on the Open Barrel
    Put Open Barrel and Steel Tank to sleep for 1 second if there is no valid output
    Fix Open Barrel OSD when holding valid capability handler
    Formatting on the Creative Barrel

--- src/main/java/mctmods/immersivetechnology/common/blocks/metal/tileentities/TileEntityBarrelOpen.java
+++ src/main/java/mctmods/immersivetechnology/common/blocks/metal/tileentities/TileEntityBarrelOpen.java
@@ -1,13 +1,11 @@
 package mctmods.immersivetechnology.common.blocks.metal.tileentities;
 
 import blusunrize.immersiveengineering.api.Lib;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IBlockOverlayText;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IComparatorOverride;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IPlayerInteraction;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.ITileDrop;
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces;
 import blusunrize.immersiveengineering.common.blocks.TileEntityIEBase;
 import blusunrize.immersiveengineering.common.util.ChatUtils;
 import blusunrize.immersiveengineering.common.util.Utils;
+import mctmods.immersivetechnology.common.util.ITFluidTank;
 import mctmods.immersivetechnology.common.util.TranslationKey;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -23,7 +21,6 @@ import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
@@ -32,14 +29,13 @@ import net.minecraftforge.fluids.capability.IFluidTankProperties;
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public class TileEntityBarrelOpen extends TileEntityIEBase implements ITickable, IBlockOverlayText, IPlayerInteraction, ITileDrop, IComparatorOverride {
+public class TileEntityBarrelOpen extends TileEntityIEBase implements ITickable, IEBlockInterfaces.IBlockOverlayText, IEBlockInterfaces.IPlayerInteraction, IEBlockInterfaces.ITileDrop, IEBlockInterfaces.IComparatorOverride, ITFluidTank.TankListener {
 
-	public FluidTank tank = new FluidTank(12000);
-
-	public static final int IGNITION_TEMPERATURE = 573;
+	public ITFluidTank tank = new ITFluidTank(12000, this);
 
 	private int acceptedAmount = 0;
 	private int lastRandom = 0;
+	private int sleep = 0;
 
 	SidedFluidHandler[] sidedFluidHandler = {new SidedFluidHandler(this, EnumFacing.DOWN), new SidedFluidHandler(this, EnumFacing.UP)};
 	SidedFluidHandler nullsideFluidHandler = new SidedFluidHandler(this, null);
@@ -68,47 +64,49 @@ public class TileEntityBarrelOpen extends TileEntityIEBase implements ITickable,
 		if(!toItem || write) nbt.setTag("tank", tankTag);
 	}
 
-	public void efficientMarkDirty() { // !!!!!!! only use it within update() function !!!!!!!
-		world.getChunkFromBlockCoords(this.getPos()).markDirty();
-	}
-
 	@Override
 	public void update() {
 		if(world.isRemote) return;
-		boolean update = false;
-		int random = 1 + RANDOM.nextInt(100);
-		if(random == lastRandom) {
-			if(tank.getFluid() == null || tank.getFluid().getFluid() == FluidRegistry.WATER) {
-				float temp = world.getBiomeProvider().getTemperatureAtHeight(world.getBiome(pos).getTemperature(pos), pos.getY());
-				if(world.isRaining() && world.canSeeSky(pos) && temp > 0.05F && temp < 2.0F) {
-					int amount = 100;
-					if(world.isThundering()) amount = 200;
-					this.tank.fill(new FluidStack(FluidRegistry.WATER, amount), true);
-					update = true;
-				} else if(temp >= 2.0F) {
-					this.tank.drain(100, true);
-					update = true;
+		if(tank.getFluidAmount() != tank.getCapacity()) {
+			int random = 1 + RANDOM.nextInt(100);
+			if(random == lastRandom) {
+				if(tank.getFluid() == null || tank.getFluid().getFluid() == FluidRegistry.WATER) {
+					float temp = world.getBiomeProvider().getTemperatureAtHeight(world.getBiome(pos).getTemperature(pos), pos.getY());
+					if(world.isRaining() && world.canSeeSky(pos) && temp > 0.05F && temp < 2.0F) {
+						int amount = 100;
+						if(world.isThundering()) amount = 200;
+						tank.fill(new FluidStack(FluidRegistry.WATER, amount), true);
+					} else if(temp >= 2.0F) {
+						tank.drain(100, true);
+					}
 				}
 			}
+			lastRandom = random;
 		}
-		lastRandom = random;
 		if(tank.getFluidAmount() > 0) {
 			EnumFacing face = EnumFacing.DOWN;
-			IFluidHandler output = FluidUtil.getFluidHandler(world, pos.offset(face), face.getOpposite());
-			if(output != null) {
-				FluidStack accepted = Utils.copyFluidStackWithAmount(tank.getFluid(), Math.min(40, tank.getFluidAmount()), false);
-				accepted.amount = output.fill(Utils.copyFluidStackWithAmount(accepted, accepted.amount, true), false);
-				if(accepted.amount > 0) {
-					int drained = output.fill(Utils.copyFluidStackWithAmount(accepted, accepted.amount, false), true);
-					this.tank.drain(drained, true);
-					update=true;
-				}
-			}
+			IFluidHandler output = FluidUtil.getFluidHandler(world, getPos().offset(face), face.getOpposite());
+            if(output != null) {
+                if(sleep == 0) {
+                    FluidStack accepted = Utils.copyFluidStackWithAmount(tank.getFluid(), Math.min(40, tank.getFluidAmount()), false);
+                    accepted.amount = output.fill(Utils.copyFluidStackWithAmount(accepted, accepted.amount, true), false);
+                	if(accepted.amount > 0) {
+                		int drained = output.fill(Utils.copyFluidStackWithAmount(accepted, accepted.amount, false), true);
+                		tank.drain(drained, true);
+                		sleep = 0;
+                	} else {
+                		sleep = 20;
+                	}
+                } else {
+                	sleep--;
+                }
+            }
 		}
-		if(update) {
-			efficientMarkDirty();
-			this.markContainingBlockForUpdate(null);
-		}
+	}
+
+	@Override
+	public void TankContentsChanged() {
+		this.markContainingBlockForUpdate(null);
 	}
 
 	@Override
@@ -162,10 +160,6 @@ public class TileEntityBarrelOpen extends TileEntityIEBase implements ITickable,
 		public int fill(FluidStack resource, boolean doFill) {
 			if(resource == null || facing != EnumFacing.UP || !barrel.isFluidValid(resource)) return 0;
 			int input = barrel.tank.fill(resource, doFill);
-			if(input > 0) {
-				barrel.markDirty();
-				barrel.markContainingBlockForUpdate(null);
-			}
 			return input;
 		}
 
@@ -179,10 +173,6 @@ public class TileEntityBarrelOpen extends TileEntityIEBase implements ITickable,
 		public FluidStack drain(int maxDrain, boolean doDrain) {
 			if(facing != EnumFacing.DOWN) return null;
 			FluidStack fluid = barrel.tank.drain(maxDrain, doDrain);
-			if(fluid != null && fluid.amount > 0) {
-				barrel.markDirty();
-				barrel.markContainingBlockForUpdate(null);
-			}
 			return fluid;
 		}
 
@@ -200,8 +190,6 @@ public class TileEntityBarrelOpen extends TileEntityIEBase implements ITickable,
 			return true;
 		}
 		if(FluidUtil.interactWithFluidHandler(player, hand, tank)) {
-			efficientMarkDirty();
-			this.markContainingBlockForUpdate(null);
 			return true;
 		}
 		return false;
