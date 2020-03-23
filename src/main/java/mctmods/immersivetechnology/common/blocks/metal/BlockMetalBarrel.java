22a958f436119b2b13538bfb0543ea149a3da0b1
tgstyle <tg.style@gmail.com>
2020-03-23 17:12:21 -0600

    Make top/bottom sides of barrel configurable
    Still needs accompanying model

--- src/main/java/mctmods/immersivetechnology/common/blocks/metal/BlockMetalBarrel.java
+++ src/main/java/mctmods/immersivetechnology/common/blocks/metal/BlockMetalBarrel.java
@@ -1,5 +1,6 @@
 package mctmods.immersivetechnology.common.blocks.metal;
 
+import blusunrize.immersiveengineering.api.IEProperties;
 import mctmods.immersivetechnology.common.blocks.BlockITTileProvider;
 import mctmods.immersivetechnology.common.blocks.ItemBlockITBase;
 import mctmods.immersivetechnology.common.blocks.metal.tileentities.TileEntityBarrel;
@@ -10,17 +11,23 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockMetalBarrel extends BlockITTileProvider<BlockType_MetalBarrel> {
 	public BlockMetalBarrel() {
-		super("metal_barrel", Material.IRON, PropertyEnum.create("type", BlockType_MetalBarrel.class), ItemBlockITBase.class);
+		super("metal_barrel", Material.IRON, PropertyEnum.create("type", BlockType_MetalBarrel.class), ItemBlockITBase.class, IEProperties.SIDECONFIG[0], IEProperties.SIDECONFIG[1]);
 		this.setHardness(3.0F);
 		this.setResistance(15.0F);
 		lightOpacity = 0;
 		this.setAllNotNormalBlock();
+	}
+
+	@Override
+	protected EnumFacing getDefaultFacing() {
+		return EnumFacing.UP;
 	}
 
 	@Override
