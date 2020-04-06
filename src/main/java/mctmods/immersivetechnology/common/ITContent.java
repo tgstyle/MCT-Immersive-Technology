5b9257893ddcb82911f06d796135a15cf090d5ed
tgstyle <tg.style@gmail.com>
2020-04-05 21:39:46 -0600

    More Steam tweaks

--- src/main/java/mctmods/immersivetechnology/common/ITContent.java
+++ src/main/java/mctmods/immersivetechnology/common/ITContent.java
@@ -236,7 +236,7 @@ public class ITContent {
 			SolarTowerRecipe.addRecipe(new FluidStack(FluidRegistry.getFluid("steam"), 750), new FluidStack(FluidRegistry.getFluid("distwater"), 250), 20);
 		}
 		if(Multiblock.enable_steamTurbine && Recipes.register_steamTurbine_recipes) {
-			SteamTurbineRecipe.addFuel(new FluidStack(FluidRegistry.WATER, 50), new FluidStack(FluidRegistry.getFluid("steam"), 500), 3);
+			SteamTurbineRecipe.addFuel(new FluidStack(FluidRegistry.WATER, 10), new FluidStack(FluidRegistry.getFluid("steam"), 100), 1);
 		}
 	}
 
