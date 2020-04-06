21e3de75c523ddb4e0ea93e749f50e24e3345a31
tgstyle <tg.style@gmail.com>
2020-04-06 10:55:53 -0600

    Code Formatting on ITContent

--- src/main/java/mctmods/immersivetechnology/common/ITContent.java
+++ src/main/java/mctmods/immersivetechnology/common/ITContent.java
@@ -106,9 +106,7 @@ public class ITContent {
 		blockMetalBarrel = new BlockMetalBarrel();
 		blockValve = new BlockValve();
 
-		if(Multiblock.enable_advancedCokeOven) {
-			blockMetalDevice = new BlockMetalDevice();
-		}
+		if(Multiblock.enable_advancedCokeOven) blockMetalDevice = new BlockMetalDevice();
 
 		/*STONE*/
 		blockStoneDecoration = new BlockStoneDecoration();
@@ -215,12 +213,8 @@ public class ITContent {
 			BoilerRecipe.addRecipe(new FluidStack(FluidRegistry.getFluid("steam"), 500), new FluidStack(FluidRegistry.WATER, 250), 10);
 			BoilerRecipe.addRecipe(new FluidStack(FluidRegistry.getFluid("steam"), 750), new FluidStack(FluidRegistry.getFluid("distwater"), 250), 10);
 			BoilerRecipe.addFuel(new FluidStack(FluidRegistry.getFluid("biodiesel"), 10), 1, 10);
-			if(FluidRegistry.getFluid("gasoline") != null) {
-				BoilerRecipe.addFuel(new FluidStack(FluidRegistry.getFluid("gasoline"), 50), 1, 10);
-			}
-			if(FluidRegistry.getFluid("diesel") != null) {
-				BoilerRecipe.addFuel(new FluidStack(FluidRegistry.getFluid("diesel"), 7), 1, 10);
-			}
+			if(FluidRegistry.getFluid("gasoline") != null) BoilerRecipe.addFuel(new FluidStack(FluidRegistry.getFluid("gasoline"), 50), 1, 10);
+			if(FluidRegistry.getFluid("diesel") != null) BoilerRecipe.addFuel(new FluidStack(FluidRegistry.getFluid("diesel"), 7), 1, 10);
 		}
 		if(Multiblock.enable_distiller && Recipes.register_distiller_recipes) {
 			ResourceLocation distillerItemName = new ResourceLocation(Distiller.distiller_output_item);
@@ -238,9 +232,7 @@ public class ITContent {
 			SolarTowerRecipe.addRecipe(new FluidStack(FluidRegistry.getFluid("steam"), 500), new FluidStack(FluidRegistry.WATER, 250), 20);
 			SolarTowerRecipe.addRecipe(new FluidStack(FluidRegistry.getFluid("steam"), 750), new FluidStack(FluidRegistry.getFluid("distwater"), 250), 20);
 		}
-		if(Multiblock.enable_steamTurbine && Recipes.register_steamTurbine_recipes) {
-			SteamTurbineRecipe.addFuel(new FluidStack(FluidRegistry.WATER, 10), new FluidStack(FluidRegistry.getFluid("steam"), 100), 1);
-		}
+		if(Multiblock.enable_steamTurbine && Recipes.register_steamTurbine_recipes) SteamTurbineRecipe.addFuel(new FluidStack(FluidRegistry.WATER, 10), new FluidStack(FluidRegistry.getFluid("steam"), 100), 1);
 	}
 
 	@SuppressWarnings("deprecation")
