5808947384160a53ec10d7cccd5f64ced4d55023
Kurtchekov <kurtchekov@gmail.com>
2020-04-07 08:34:10 +0300

    Disabling multiblocks now also hides it from JEI

--- src/main/java/mctmods/immersivetechnology/common/util/compat/jei/GenericMultiblockIngredient.java
+++ src/main/java/mctmods/immersivetechnology/common/util/compat/jei/GenericMultiblockIngredient.java
@@ -4,6 +4,7 @@ import mctmods.immersivetechnology.common.ITContent;
 import mctmods.immersivetechnology.common.blocks.metal.types.BlockType_MetalMultiblock;
 import mctmods.immersivetechnology.common.blocks.stone.types.BlockType_StoneMultiblock;
 import net.minecraft.item.ItemStack;
+import mctmods.immersivetechnology.common.Config.ITConfig.Machines.Multiblock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +13,19 @@ public class GenericMultiblockIngredient {
 
     public static List<GenericMultiblockIngredient> list = new ArrayList<>();
 
-    public static GenericMultiblockIngredient STEAM_TURBINE = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.STEAM_TURBINE.getMeta()));
-    public static GenericMultiblockIngredient DISTILLER = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.DISTILLER.getMeta()));
-    public static GenericMultiblockIngredient SOLAR_TOWER = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.SOLAR_TOWER.getMeta()));
-    public static GenericMultiblockIngredient BOILER = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.BOILER.getMeta()));
-    public static GenericMultiblockIngredient COKE_OVEN_ADVANCED = new GenericMultiblockIngredient(new ItemStack(ITContent.blockStoneMultiblock, 1, BlockType_StoneMultiblock.COKE_OVEN_ADVANCED.getMeta()));
+    public static GenericMultiblockIngredient STEAM_TURBINE;
+    public static GenericMultiblockIngredient DISTILLER;
+    public static GenericMultiblockIngredient SOLAR_TOWER;
+    public static GenericMultiblockIngredient BOILER;
+    public static GenericMultiblockIngredient COKE_OVEN_ADVANCED;
+
+    static {
+        if (Multiblock.enable_steamTurbine) STEAM_TURBINE = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.STEAM_TURBINE.getMeta()));
+        if (Multiblock.enable_distiller) DISTILLER = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.DISTILLER.getMeta()));
+        if (Multiblock.enable_solarTower) SOLAR_TOWER = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.SOLAR_TOWER.getMeta()));
+        if (Multiblock.enable_boiler) BOILER = new GenericMultiblockIngredient(new ItemStack(ITContent.blockMetalMultiblock, 1, BlockType_MetalMultiblock.BOILER.getMeta()));
+        if (Multiblock.enable_advancedCokeOven) COKE_OVEN_ADVANCED = new GenericMultiblockIngredient(new ItemStack(ITContent.blockStoneMultiblock, 1, BlockType_StoneMultiblock.COKE_OVEN_ADVANCED.getMeta()));
+    }
 
     public ItemStack renderStack;
 
