aff234817b13270e1875422a0aa0b66e02ff23b9
allanbraun <allanvobraun@gmail.com>
2021-02-21 17:07:12 -0300

    Replaced SideOnly to the new OnlyIn

--- src/main/java/mctmods/immersivetechnology/common/items/ItemITBase.java
+++ src/main/java/mctmods/immersivetechnology/common/items/ItemITBase.java
@@ -3,12 +3,12 @@ package mctmods.immersivetechnology.common.items;
 import blusunrize.immersiveengineering.common.items.IEItemInterfaces.IColouredItem;
 import mctmods.immersivetechnology.ImmersiveTechnology;
 import mctmods.immersivetechnology.common.ITContent;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 	/*
 	* @author BluSunrize
@@ -35,8 +35,8 @@ public class ItemITBase extends Item implements IColouredItem {
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
-	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> list) {
+	@OnlyIn(Dist.CLIENT)
+	public void getSubItems(ItemGroup tab, NonNullList<ItemStack> list) {
 		if(this.isInCreativeTab(tab)) {
 			if(getSubNames() != null) {
 				for(int i = 0; i < getSubNames().length; i++) {
