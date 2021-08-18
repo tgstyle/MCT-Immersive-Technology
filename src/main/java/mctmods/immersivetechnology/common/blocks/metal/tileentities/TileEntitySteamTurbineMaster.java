61a54adec1572430a7839bc7b898357a081362db
Silfryi Kalsandryn <silfryikalsandryn@protonmail.com>
2021-08-17 21:50:25 -0400

    First pass for steam turbine output

--- src/main/java/mctmods/immersivetechnology/common/blocks/metal/tileentities/TileEntitySteamTurbineMaster.java
+++ src/main/java/mctmods/immersivetechnology/common/blocks/metal/tileentities/TileEntitySteamTurbineMaster.java
@@ -80,7 +80,7 @@ public class TileEntitySteamTurbineMaster extends TileEntitySteamTurbineSlave im
 
 	private void pumpOutputOut() {
 		if(tanks[1].getFluidAmount() == 0) return;
-		if(fluidOutputPos == null) fluidOutputPos = ITUtils.LocalOffsetToWorldBlockPos(this.getPos(), 0, 2, 8, facing, mirrored);
+		if(fluidOutputPos == null) fluidOutputPos = ITUtils.LocalOffsetToWorldBlockPos(this.getPos(), 0, -1, 9, facing, mirrored);
 		IFluidHandler output = FluidUtil.getFluidHandler(world, fluidOutputPos, facing.getOpposite());
 		if(output == null) return;
 		FluidStack out = tanks[1].getFluid();
