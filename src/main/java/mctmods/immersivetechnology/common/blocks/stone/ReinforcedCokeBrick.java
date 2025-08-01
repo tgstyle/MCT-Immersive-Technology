cde5aa973146a55493bdc32c62e4050eb6a535b9
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-08-01 09:16:02 -0600

    Fix up advanced coke oven block recipe

--- src/main/java/mctmods/immersivetechnology/common/blocks/stone/ReinforcedCokeBrick.java
+++ src/main/java/mctmods/immersivetechnology/common/blocks/stone/ReinforcedCokeBrick.java
@@ -2,10 +2,8 @@ package mctmods.immersivetechnology.common.blocks.stone;
 
 import net.minecraft.world.level.block.Block;
 
-public class ReinforcedCokeBrick extends Block
-{
-    public ReinforcedCokeBrick(Properties pProperties)
-    {
+public class ReinforcedCokeBrick extends Block {
+    public ReinforcedCokeBrick(Properties pProperties) {
         super(pProperties);
     }
 }
