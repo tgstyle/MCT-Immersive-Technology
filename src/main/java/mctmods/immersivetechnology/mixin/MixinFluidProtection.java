bc6f2f0f9390e61bab494f107cd20026d1a80faa
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-09-16 09:36:52 -0600

    Mixin cleanup

--- src/main/java/mctmods/immersivetechnology/mixin/MixinFluidProtection.java
+++ src/main/java/mctmods/immersivetechnology/mixin/MixinFluidProtection.java
@@ -14,29 +14,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.function.Supplier;
 
 @Mixin(value = IEBlocks.class, remap = false)
-public class MixinFluidProtection
-{
+public class MixinFluidProtection {
     @Shadow @Final @Mutable
-    private static Supplier<BlockBehaviour.Properties> METAL_PROPERTIES_NO_OCCLUSION;
+    public static Supplier<BlockBehaviour.Properties> METAL_PROPERTIES_NO_OCCLUSION;
 
     @Shadow @Final
     private static Supplier<BlockBehaviour.Properties> METAL_PROPERTIES_NO_OVERLAY;
 
-    @Inject(
-            method = "<clinit>",  // Target the static initializer method (<clinit>)
-            at = @At("RETURN")   // Inject after the static initialization logic
-    )
+    @Inject(method = "<clinit>", at = @At("RETURN"))
     private static void injectAfterStaticInitialization(CallbackInfo ci) {
-        // quick debug statement
         ITLib.IT_LOGGER.info("Mixin applied: IEBlocks <clinit> method called.");
         ITLib.IT_LOGGER.info("Original METAL_PROPERTIES_NO_OVERLAY: {}", METAL_PROPERTIES_NO_OVERLAY.get());
-
-        // replace the static value
-
-        METAL_PROPERTIES_NO_OCCLUSION = () -> METAL_PROPERTIES_NO_OVERLAY.get() // directly referenced from IE's code under Blu's License of Common Sense. See https://github.com/BluSunrize/ImmersiveEngineering/issues/5906 and the exact source of the code, https://github.com/BluSunrize/ImmersiveEngineering/commit/b6fb636b616bf0c1018c5c2c0095c18bd190161d
+        METAL_PROPERTIES_NO_OCCLUSION = () -> METAL_PROPERTIES_NO_OVERLAY.get()
                 .noOcclusion()
                 .forceSolidOn();
-
         ITLib.IT_LOGGER.info("Modified METAL_PROPERTIES_NO_OCCLUSION has been set.");
     }
 }
