5d6dab6b7ce7737a779a8bdfe3f794ddff39b7b4
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-09-16 09:32:00 -0600

    MixinInitializationTracker
    Fields to synchronizedList

---
+++ src/main/java/mctmods/immersivetechnology/mixin/MixinInitializationTracker.java
@@ -0,0 +1,23 @@
+package mctmods.immersivetechnology.mixin;
+
+import blusunrize.immersiveengineering.api.utils.SetRestrictedField;
+import org.apache.commons.lang3.tuple.Pair;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Collections;
+import java.util.List;
+
+@Mixin(targets = "blusunrize.immersiveengineering.api.utils.SetRestrictedField$InitializationTracker", remap = false)
+public class MixinInitializationTracker {
+    @Shadow @Final @Mutable
+    private List<Pair<Exception, SetRestrictedField<?>>> fields;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) { this.fields = Collections.synchronizedList(this.fields); }
+}
