728d6a47344cd07e5fea72a4cbda973781f66336
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-10-12 16:29:31 -0600

    Cleanup unused import

--- src/main/java/mctmods/immersivetechnology/common/blocks/metal/BarrelOpenBlock.java
+++ src/main/java/mctmods/immersivetechnology/common/blocks/metal/BarrelOpenBlock.java
@@ -11,7 +11,6 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 import org.jetbrains.annotations.NotNull;
 import java.util.function.BiFunction;
