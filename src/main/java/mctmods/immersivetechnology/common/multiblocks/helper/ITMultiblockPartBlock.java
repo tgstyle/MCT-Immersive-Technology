76b4c3ddf9f6939a803b9f9c4d57c01698c7e193
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-11-18 14:35:35 -0700

    Fix multiblock break animation

--- src/main/java/mctmods/immersivetechnology/common/multiblocks/helper/ITMultiblockPartBlock.java
+++ src/main/java/mctmods/immersivetechnology/common/multiblocks/helper/ITMultiblockPartBlock.java
@@ -1,6 +1,10 @@
 package mctmods.immersivetechnology.common.multiblocks.helper;
 
 import blusunrize.immersiveengineering.api.multiblocks.blocks.MultiblockRegistration;
+import blusunrize.immersiveengineering.api.multiblocks.blocks.env.IMultiblockBEHelper;
+import blusunrize.immersiveengineering.api.multiblocks.blocks.env.IMultiblockBEHelperDummy;
+import blusunrize.immersiveengineering.api.multiblocks.blocks.env.IMultiblockLevel;
+import blusunrize.immersiveengineering.api.multiblocks.blocks.logic.IMultiblockBE;
 import blusunrize.immersiveengineering.api.multiblocks.blocks.logic.IMultiblockState;
 import blusunrize.immersiveengineering.api.multiblocks.blocks.registry.MultiblockPartBlock;
 import mctmods.immersivetechnology.common.blocks.helper.ITBlockInterfaces;
@@ -8,14 +12,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
+@SuppressWarnings("deprecation")
 public class ITMultiblockPartBlock<S extends IMultiblockState> extends MultiblockPartBlock<S> {
     public ITMultiblockPartBlock(Properties properties, MultiblockRegistration<S> multiblock) { super(properties, multiblock); }
 
@@ -25,10 +33,25 @@ public class ITMultiblockPartBlock<S extends IMultiblockState> extends Multibloc
         BlockEntity te = level.getBlockEntity(pos);
         if (te instanceof ITBlockInterfaces.IPlayerInteraction be) {
             Vec3 hitVec = hit.getLocation().subtract(pos.getX(), pos.getY(), pos.getZ());
-            if (be.interact(hit.getDirection(), player, hand, player.getItemInHand(hand), (float) hitVec.x, (float) hitVec.y, (float) hitVec.z)) {
-                return InteractionResult.sidedSuccess(level.isClientSide);
-            }
+            if (be.interact(hit.getDirection(), player, hand, player.getItemInHand(hand), (float) hitVec.x, (float) hitVec.y, (float) hitVec.z)) { return InteractionResult.sidedSuccess(level.isClientSide); }
         }
         return super.use(state, level, pos, player, hand, hit);
+    }
+
+    @Override
+    public float getDestroyProgress(@NotNull BlockState state, @NotNull Player player, @NotNull BlockGetter worldIn, @NotNull BlockPos pos) {
+        if (!(worldIn instanceof Level level)) { return super.getDestroyProgress(state, player, worldIn, pos); }
+        BlockEntity te = level.getBlockEntity(pos);
+        if (!(te instanceof IMultiblockBE<?> mbe)) { return super.getDestroyProgress(state, player, worldIn, pos); }
+        IMultiblockBEHelper<?> helper = mbe.getHelper();
+        if (helper instanceof IMultiblockBEHelperDummy<?> dummy) {
+            IMultiblockLevel mbLevel = Objects.requireNonNull(helper.getContext()).getLevel();
+            BlockPos relPos = dummy.getPositionInMB();
+            BlockPos offset = mbLevel.getOrientation().getAbsoluteOffset(relPos);
+            BlockPos masterPos = pos.subtract(offset);
+            BlockState masterState = level.getBlockState(masterPos);
+            return masterState.getDestroyProgress(player, worldIn, masterPos);
+        }
+        return super.getDestroyProgress(state, player, worldIn, pos);
     }
 }
