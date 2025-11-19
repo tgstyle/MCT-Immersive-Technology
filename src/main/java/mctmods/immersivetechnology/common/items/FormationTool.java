c6805355041109b06521124cf54a201d85090001
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-11-18 19:02:52 -0700

    Formation tool cleanup

--- src/main/java/mctmods/immersivetechnology/common/items/FormationTool.java
+++ src/main/java/mctmods/immersivetechnology/common/items/FormationTool.java
@@ -5,9 +5,9 @@ import blusunrize.immersiveengineering.api.Lib;
 import blusunrize.immersiveengineering.api.client.TextUtils;
 import blusunrize.immersiveengineering.api.multiblocks.MultiblockHandler;
 import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
-import blusunrize.immersiveengineering.common.util.advancements.IEAdvancements;
-import blusunrize.immersiveengineering.common.util.orientation.RotationUtil;
 import mctmods.immersivetechnology.common.blocks.helper.ITBlockInterfaces;
+import mctmods.immersivetechnology.common.util.ITAdvancements;
+import mctmods.immersivetechnology.common.util.ITRotationUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
@@ -21,20 +21,16 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.common.Tags;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
@@ -103,7 +99,7 @@ public class FormationTool extends Item {
                 if (MultiblockHandler.postMultiblockFormationEvent(player, mb, pos, stack).isCanceled()) { continue; }
                 boolean formed = mb.createStructure(world, pos, multiblockSide, player);
                 if (formed) {
-                    if (player instanceof ServerPlayer sPlayer) { IEAdvancements.TRIGGER_MULTIBLOCK.trigger(sPlayer, mb, stack); }
+                    if (player instanceof ServerPlayer sPlayer) { ITAdvancements.TRIGGER_MULTIBLOCK.trigger(sPlayer, mb, stack); }
                     return InteractionResult.SUCCESS;
                 }
             }
@@ -120,7 +116,7 @@ public class FormationTool extends Item {
             if (!rotate && tile instanceof ITBlockInterfaces.IDirectionalBE dirBE) {
                 assert player != null;
                 rotate = dirBE.canHammerRotate(side, context.getClickLocation().subtract(Vec3.atLowerCornerOf(pos)), player); }
-            if (rotate && RotationUtil.rotateBlock(world, pos, player != null && (player.isShiftKeyDown() != side.equals(Direction.DOWN)))) { return InteractionResult.SUCCESS; }
+            if (rotate && ITRotationUtil.rotateBlock(world, pos, player != null && (player.isShiftKeyDown() != side.equals(Direction.DOWN)))) { return InteractionResult.SUCCESS; }
             else if (!rotate && tile instanceof ITBlockInterfaces.IHammerInteraction hammerInteraction) {
                 assert player != null;
                 if (hammerInteraction.hammerUseSide(side, player, context.getHand(), context.getClickLocation())) { return InteractionResult.SUCCESS; }
@@ -149,10 +145,7 @@ public class FormationTool extends Item {
 
     @Override
     public @NotNull InteractionResult interactLivingEntity(@NotNull ItemStack stack, @NotNull Player player, @NotNull LivingEntity entity, @NotNull InteractionHand hand) {
-        if (!player.level().isClientSide) {
-            RotationUtil.rotateEntity(entity, player);
-            if (entity instanceof ArmorStand) { return InteractionResult.SUCCESS; }
-        }
+        if (!player.level().isClientSide && ITRotationUtil.rotateEntity(entity)) { return InteractionResult.SUCCESS; }
         return InteractionResult.PASS;
     }
 
@@ -168,21 +161,15 @@ public class FormationTool extends Item {
     }
 
     @Override
-    public boolean isEnchantable(@NotNull ItemStack stack) { return true; }
+    public boolean isEnchantable(@NotNull ItemStack stack) { return false; }
 
     @Override
-    public int getEnchantmentValue(ItemStack stack) { return 14; }
+    public int getEnchantmentValue(ItemStack stack) { return 0; }
 
     @Override
-    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
-        return EnchantmentHelper.getEnchantments(book).keySet().stream().allMatch(enchantment -> canApplyAtEnchantingTable(stack, enchantment));
-    }
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) { return false; }
 
     @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
-        return enchantment == Enchantments.BLOCK_EFFICIENCY || enchantment == Enchantments.UNBREAKING || enchantment == Enchantments.MENDING;
-    }
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) { return false; }
 
-    @Override
-    public boolean isValidRepairItem(@NotNull ItemStack stack, ItemStack repairCandidate) { return repairCandidate.is(Tags.Items.INGOTS_IRON); }
 }
---
+++ src/main/java/mctmods/immersivetechnology/common/util/ITAdvancements.java
@@ -0,0 +1,8 @@
+package mctmods.immersivetechnology.common.util;
+
+import blusunrize.immersiveengineering.api.multiblocks.MultiblockAdvancementTrigger;
+import blusunrize.immersiveengineering.common.util.advancements.IEAdvancements;
+
+public class ITAdvancements {
+    public static final MultiblockAdvancementTrigger TRIGGER_MULTIBLOCK = IEAdvancements.TRIGGER_MULTIBLOCK;
+}
