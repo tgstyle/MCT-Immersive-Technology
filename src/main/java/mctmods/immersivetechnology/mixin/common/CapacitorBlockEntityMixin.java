df523f70fbf012fd1d8ebf84b114a826db563804
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-11-25 20:27:27 -0700

    Mixin to allow wrench to configure IE capacitors

---
+++ src/main/java/mctmods/immersivetechnology/mixin/common/CapacitorBlockEntityMixin.java
@@ -0,0 +1,23 @@
+package mctmods.immersivetechnology.mixin.common;
+
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces;
+import blusunrize.immersiveengineering.common.blocks.metal.CapacitorBlockEntity;
+import blusunrize.immersiveengineering.common.util.Utils;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(CapacitorBlockEntity.class)
+public abstract class CapacitorBlockEntityMixin implements IEBlockInterfaces.IPlayerInteraction {
+    @Override
+    public boolean interact(@NotNull Direction side, @NotNull Player player, @NotNull InteractionHand hand, @NotNull ItemStack heldItem, float hitX, float hitY, float hitZ) {
+        if (Utils.isHammer(heldItem)) {
+            Direction activeSide = player.isShiftKeyDown() ? side.getOpposite() : side;
+            return ((IEBlockInterfaces.IConfigurableSides) this).toggleSide(activeSide, player);
+        }
+        return false;
+    }
+}
