444ef9d51e059050fdadcb6a96f87627bf867ef2
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-12-10 18:40:57 -0700

    Mixin to reveal crash on early server load

--- src/main/java/mctmods/immersivetechnology/mixin/MixinMCTMinecraftServer.java
+++ src/main/java/mctmods/immersivetechnology/mixin/MixinMCTMinecraftServer.java
@@ -5,22 +5,34 @@ import net.minecraft.server.MinecraftServer;
 import org.apache.logging.log4j.Logger;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import mctmods.immersivetechnology.core.MCTMixinConfig;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.util.Objects;
 
 @Mixin(MinecraftServer.class)
 public abstract class MixinMCTMinecraftServer {
+    @Unique
+    private static String formatStackTrace$helper(Throwable t) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Objects.toString(t.getMessage(), "No message")).append("\n");
+        for (StackTraceElement ste : t.getStackTrace()) { sb.append("\tat ").append(ste).append("\n"); }
+        Throwable cause = t.getCause();
+        if (cause != null) {
+            sb.append("Caused by: ").append(formatStackTrace$helper(cause));
+        }
+        return sb.toString();
+    }
+
     @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V", remap = false), remap = false)
-    private void redirectErrorLog(Logger logger, String message, Throwable t) {
+    @Unique
+    private void redirectErrorLog$helper(Logger logger, String message, Throwable t) {
         if (MCTMixinConfig.mixinSettings.enableErrorLoggingRedirect) {
-            StringWriter sw = new StringWriter();
-            t.printStackTrace(new PrintWriter(sw));
-            logger.error("{}: {}\n{}", message, t.getMessage(), sw.toString());
+            String full = message + ": " + formatStackTrace$helper(t);
+            System.err.print(full);
         } else {
             logger.error(message, t);
         }
