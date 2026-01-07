24dd485fa302a29d6c1aa82aa56b8167cecc6a81
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-01-07 09:58:42 -0700

    Update variable name

--- src/main/java/mctmods/immersivetechnology/mixin/client/BlockRenderDispatcherMixin.java
+++ src/main/java/mctmods/immersivetechnology/mixin/client/BlockRenderDispatcherMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BlockRenderDispatcher.class)
 public abstract class BlockRenderDispatcherMixin {
-    @ModifyVariable(method = "renderBreakingTexture(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/BlockAndTintGetter;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraftforge/client/model/data/ModelData;)V", at = @At(value = "STORE", ordinal = 0), ordinal = 0, remap = false)
+    @ModifyVariable(method = "renderBreakingTexture(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/BlockAndTintGetter;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraftforge/client/model/data/ModelData;)V", at = @At(value = "STORE", ordinal = 0), remap = false, name = "bakedmodel")
     private BakedModel it$useFullBlockModelForDamage(BakedModel model, BlockState pState, BlockPos pPos, BlockAndTintGetter pLevel) {
         if (pState.getBlock() instanceof ITMultiblockPartBlock) {
             BlockModelShaper modelShapes = Minecraft.getInstance().getBlockRenderer().getBlockModelShaper();
