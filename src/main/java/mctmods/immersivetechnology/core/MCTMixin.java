5a75c40de248e300b855d50c8577a32c6e5f1f21
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-01-31 18:54:50 -0700

    Fix mixin when booting with jar

--- src/main/java/mctmods/immersivetechnology/core/MCTMixin.java
+++ src/main/java/mctmods/immersivetechnology/core/MCTMixin.java
@@ -1,12 +1,13 @@
 package mctmods.immersivetechnology.core;
 
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.common.config.Config.Type;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import org.spongepowered.asm.mixin.Mixins;
 
 @Mod(modid = "mct_mixin", name = "MCT Mixin", version = "1.0", acceptedMinecraftVersions = "[1.12.2]")
 public class MCTMixin {
@@ -14,12 +15,19 @@ public class MCTMixin {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        Mixins.addConfiguration("mixins.immersiveengineering.json");
-        Mixins.addConfiguration("mixins.immersivetechnology.json");
+        ConfigManager.sync("mct_mixin", Type.INSTANCE);
+
         Thread.setDefaultUncaughtExceptionHandler((thread, t) -> {
             System.err.println("Uncaught exception in thread " + thread.getName() + ":");
             t.printStackTrace(System.err);
         });
-        LOGGER.info("Loaded config: enableAdditionsLogging={}, enablePotentialsLogging={}, enableDevSided={}", MCTMixinConfig.mixinSettings.enableAdditionsLogging, MCTMixinConfig.mixinSettings.enablePotentialsLogging, MCTMixinConfig.mixinSettings.enableDevSided);
+
+        LOGGER.info("Loaded config: enableAdditionsLogging={}, enablePotentialsLogging={}, enableDevSided={}, enableWorldMixin={}, replace_IE_pipes={}, enableErrorLoggingRedirect={}",
+                MCTMixinConfig.mixinSettings.enableAdditionsLogging,
+                MCTMixinConfig.mixinSettings.enablePotentialsLogging,
+                MCTMixinConfig.mixinSettings.enableDevSided,
+                MCTMixinConfig.mixinSettings.enableWorldMixin,
+                MCTMixinConfig.mixinSettings.replace_IE_pipes,
+                MCTMixinConfig.mixinSettings.enableErrorLoggingRedirect);
     }
 }
