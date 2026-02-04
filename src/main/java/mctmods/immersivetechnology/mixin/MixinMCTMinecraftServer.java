7cbd90b8572fffa041d095bfa9b1f3c63a8ccc5b
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-02-03 20:51:40 -0700

    Fix for PaperFix

--- src/main/java/mctmods/immersivetechnology/mixin/MixinMCTMinecraftServer.java
+++ src/main/java/mctmods/immersivetechnology/mixin/MixinMCTMinecraftServer.java
@@ -1,24 +1,25 @@
 package mctmods.immersivetechnology.mixin;
 
+import mctmods.immersivetechnology.core.MCTMixinConfig;
 import net.minecraft.server.MinecraftServer;
-
 import org.apache.logging.log4j.Logger;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import mctmods.immersivetechnology.core.MCTMixinConfig;
-
 import java.util.Objects;
 
-@Mixin(MinecraftServer.class)
+@Mixin(value = MinecraftServer.class, priority = 1500)
 public abstract class MixinMCTMinecraftServer {
-    @Unique private static String formatStackTrace$helper(Throwable t) {
+
+    @Unique
+    private static String formatStackTrace$helper(Throwable t) {
         StringBuilder sb = new StringBuilder();
         sb.append(Objects.toString(t.getMessage(), "No message")).append("\n");
-        for (StackTraceElement ste : t.getStackTrace()) { sb.append("\tat ").append(ste).append("\n"); }
+        for (StackTraceElement ste : t.getStackTrace()) {
+            sb.append("\tat ").append(ste).append("\n");
+        }
         Throwable cause = t.getCause();
         if (cause != null) {
             sb.append("Caused by: ").append(formatStackTrace$helper(cause));
@@ -26,13 +27,15 @@ public abstract class MixinMCTMinecraftServer {
         return sb.toString();
     }
 
-    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V", ordinal = 0, remap = false))
-    @Unique private void redirectErrorLog$helper(Logger logger, String message, Throwable t) {
+    @Redirect(method = "run",
+            at = @At(value = "INVOKE",
+                    target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V",
+                    ordinal = 0))
+    private void redirectErrorLog$helper(Logger logger, String message, Throwable t) {
+        logger.error(message, t);
         if (MCTMixinConfig.mixinSettings.enableErrorLoggingRedirect) {
             String full = message + ": " + formatStackTrace$helper(t);
             System.err.print(full);
-        } else {
-            logger.error(message, t);
         }
     }
 }
