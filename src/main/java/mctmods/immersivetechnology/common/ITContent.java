0e3cf2ada7f15d52ed1be4b79872504d407c8551
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-02-15 10:40:38 -0700

    Fix base heater IF value in manual

--- src/main/java/mctmods/immersivetechnology/common/ITContent.java
+++ src/main/java/mctmods/immersivetechnology/common/ITContent.java
@@ -417,6 +417,7 @@ public class ITContent {
     }
 
     public static void registerVariables() {
+        Config.manual_int.put("cokeOvenBaseheater_consumption", ITConfig.Multiblocks.advancedCokeOvenBaseheater.advancedCokeOvenBaseheater_energy_consumption);
         Config.manual_int.put("alternator_energyPerTickPerPort", (ITConfig.Multiblocks.alternator.alternator_energy_perTick / 6));
         Config.manual_int.put("alternator_energyStorage", ITConfig.Multiblocks.alternator.alternator_energy_capacitorSize);
         Config.manual_int.put("alternator_energyPerTick", ITConfig.Multiblocks.alternator.alternator_energy_perTick);
