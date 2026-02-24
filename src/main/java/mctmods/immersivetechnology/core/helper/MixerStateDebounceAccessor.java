ca801edd7a76ceb97b14fcfef82accbae4b0d0b5
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-02-24 15:03:09 -0700

    Rename this class for it's purpose

--- src/main/java/mctmods/immersivetechnology/core/helper/MixerStateDebounceAccessor.java
+++ src/main/java/mctmods/immersivetechnology/core/helper/MixerStateDebounceAccessor.java
@@ -3,7 +3,7 @@ package mctmods.immersivetechnology.core.helper;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 
-public interface MixerStateDebounce {
+public interface MixerStateDebounceAccessor {
 
     NonNullList<ItemStack> it$getLastComponents();
 
