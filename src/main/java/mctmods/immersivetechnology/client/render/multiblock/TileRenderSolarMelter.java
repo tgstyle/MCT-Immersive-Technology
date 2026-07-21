c363572d3152dee59b2c0fb02719375d0e0e02d7
tgstyle <5832709+tgstyle@users.noreply.github.com>
2026-07-21 17:19:14 -0600

    Add particle effects to Solar Melter

--- src/main/java/mctmods/immersivetechnology/client/render/multiblock/TileRenderSolarMelter.java
+++ src/main/java/mctmods/immersivetechnology/client/render/multiblock/TileRenderSolarMelter.java
@@ -1,13 +1,23 @@
 package mctmods.immersivetechnology.client.render.multiblock;
 
+import mctmods.immersivetechnology.api.particles.ColoredBeamRenderer;
 import mctmods.immersivetechnology.common.ITContent;
 import mctmods.immersivetechnology.common.multiblocks.metal.tileentities.TileEntitySolarMelterMaster;
 import mctmods.immersivetechnology.common.multiblocks.metal.tileentitiesmultiblockpart.TileEntityITMultiblockPartSolarMelter;
 
 import net.minecraft.block.Block;
+import net.minecraft.util.math.BlockPos;
 
 public class TileRenderSolarMelter extends TileRenderITMultiblockStatic<TileEntitySolarMelterMaster> {
     @Override protected int getTotalBlocks() { return TileEntityITMultiblockPartSolarMelter.instance.width * TileEntityITMultiblockPartSolarMelter.instance.length * TileEntityITMultiblockPartSolarMelter.instance.height; }
 
     @Override protected Block getMultiblockBlock() { return ITContent.blockMetalMultiblock1; }
+
+    @Override protected void renderDynamic(TileEntitySolarMelterMaster te, float partialTicks) {
+        if (te.particlePos0 == null) return;
+        if (te.heatLevel < TileEntitySolarMelterMaster.getWorkingHeatLevel() || te.reflectorStrength <= 0) return;
+        BlockPos offset = te.particlePos0.subtract(te.getPos());
+        ColoredBeamRenderer.renderBeam(offset.getX(), offset.getY(), offset.getZ(), 16F, partialTicks, 1F,
+                1F, 0F, 1F, 1F, 0F, 0.2F);
+    }
 }
